e3a251eb2851c30bf0e285acc70af57425f77bae
namoong0917 <namoong0917@gmail.com>
2023-11-17 17:59:07 +0900

    Refactor: Applying React Router Dom

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,34 @@
+import React from "react";
+import { Outlet, Route, Routes } from "react-router-dom";
+import MainPage from './pages/MainPage';
+import SearchedVideosPage from './pages/SearchedVideosPage';
+import VideoPage from './pages/VideoPage';
+import NavigationBar from './components/NavigationBar';
+import SideBar from './components/SideBar/index';
+
+const Layout = () => {
+  return(
+    <>
+      <NavigationBar />
+      <SideBar />
+      <main>
+        <Outlet />
+      </main>
+    </>
+  )
+}
 
 function App() {
 
   return (
     <>
-      
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<MainPage />} />
+          <Route path="/results/:input" element={<SearchedVideosPage />} />
+          <Route path="/Video/:videoId" element={<VideoPage />} />
+        </Route>
+      </Routes>
     </>
   )
 }
