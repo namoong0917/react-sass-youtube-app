38f83de68f8ec703e52f9e732f1e0af4088285a9
namoong0917 <namoong0917@gmail.com>
2023-11-20 17:01:17 +0900

    Refactor: Create sidebar

--- src/components/SideBar/SmallSideBar.jsx
+++ src/components/SideBar/SmallSideBar.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const SmallSideBar = () => {
 	return (
-		<div>SmallSideBar</div>
+		<div style={{color: 'white', marginTop:200, fontSize: 200}}>SmallSideBar</div>
 	)
 }
 
--- src/components/SideBar/index.jsx
+++ src/components/SideBar/index.jsx
@@ -1,8 +1,21 @@
 import React from 'react'
+import useWindowSize from '../../helpers/useWindowSize';
+import SmallSideBar from './SmallSideBar';
+import BigSideBar from './BigSideBar';
 
 const SideBar = () => {
+	const {width} = useWindowSize();
 	return (
-		<div>SideBar</div>
+		<>
+			{width <792 // width가 792px 보다 작을때는 null
+			? null
+			: (
+				width < 1250 // width가 1250px 보다 작을경우
+					? <SmallSideBar />
+					: <BigSideBar />
+				)
+			}
+		</>
 	)
 }
 
