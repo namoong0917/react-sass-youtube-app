408b86d64188bcbb6029e12f513ef4027840344a
namoong0917 <namoong0917@gmail.com>
2023-11-22 03:16:47 +0900

    Refactor: Import video data, save video data

--- src/pages/MainPage/index.jsx
+++ src/pages/MainPage/index.jsx
@@ -1,9 +1,46 @@
-import React from 'react'
+import React, { useCallback, useContext, useEffect, useState } from "react";
+import axios from "../../api/axios";
+import { getVideoInfo } from "../../helpers/fetchingData";
+import { SideBarContext } from "../../context/SideBarContext";
 
-const mainPage = () => {
-	return (
-		<div>mainPage</div>
-	)
-}
+const MainPage = () => {
+  const { setIsToggled } = useContext(SideBarContext);
+  const stroedVideos = JSON.parse(localStorage.getItem("mainVideos"));
+  const [mainVideos, setMainVideos] = useState(stroedVideos || []); // 정보를 담아줄 state
 
-export default mainPage
+	// 비동기 요청을 위해 async 로 감싸줌
+	// 컴포넌트가 다시 랜더링 될때 디펜던시 array[stroedVideos] 에있는게 변하지 않으면 다시 생성 못하게
+  const getMainVideos = useCallback(async () => {
+    try {
+      if (mainVideos.length === 0) {
+        // === !stroedVideos
+        // localStorage에 저장된 데이터가 없을때만 다음을 실행
+        const res = await axios.get(
+          `/search?part=snippet&maxResults=10&q=beautiful%20place`
+        );
+        let videosArray = await res.data.items;
+        videosArray = await getVideoInfo(videosArray);
+        setMainVideos(videosArray);
+
+        localStorage.setItem("mainVideos", JSON.stringify(videosArray));
+      }
+    } catch (err) {
+      console.log(err);
+    }
+  }, [stroedVideos]);
+
+  useEffect(() => {
+    // 비디오 페이지 들어가고 다시 메인페이지 돌아오면 사이드바 보이도록
+    setIsToggled(true);
+  }, []);
+
+  useEffect(() => {
+    getMainVideos();
+  }, [getMainVideos]);
+
+  return (
+    <div>MainPage</div>
+  );
+};
+
+export default MainPage;
