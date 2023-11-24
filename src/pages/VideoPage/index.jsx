fa178d76b9c38fe79137065716fe68166e89a27b
namoong0917 <namoong0917@gmail.com>
2023-11-25 00:21:27 +0900

    Refactor: Get video data

--- src/pages/VideoPage/index.jsx
+++ src/pages/VideoPage/index.jsx
@@ -1,8 +1,28 @@
 import React from 'react'
+import { useLocation, useParams } from 'react-router-dom';
+import YouTube from 'react-youtube';
 
 const VideoPage = () => {
+	const {videoId} = useParams();
+	let location = useLocation();
+	const {state: currentVideo} = location;
+
+	const onPlayerReady = (e) => {
+		e.target.playVideo();
+	}
+
+	const opts = {
+		height: '390',
+		width: '640',
+		playerVars: {
+			autoplay: 1,
+		},
+	};
+
 	return (
-		<div>VideoPage</div>
+		<div>
+			<YouTube className='youtube-player' videoId={videoId} onPlay={onPlayerReady} opts={opts} />
+		</div>
 	)
 }
 
