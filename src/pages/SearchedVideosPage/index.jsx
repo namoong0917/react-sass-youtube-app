68638c83bb28836f4ac8928132b3eb53c724b0cd
namoong0917 <namoong0917@gmail.com>
2024-01-23 22:46:57 +0900

    searchedVideosPage styling

--- src/pages/SearchedVideosPage/index.jsx
+++ src/pages/SearchedVideosPage/index.jsx
@@ -1,9 +1,37 @@
-import React from 'react'
+import React, { useCallback, useContext, useEffect, useState } from "react";
+import { SearchContext } from "../../context/SearchContext";
+import { SideBarContext } from "../../context/SideBarContext";
+import { getVideoInfo } from "../../helpers/fetchingData";
+import VideoCard from "../../components/VideoCard";
 
-const SearchedVideoPage = () => {
-	return (
-		<div>SearchedVideoPage</div>
-	)
-}
+const SearchedVideosPage = () => {
+  const [searchVideos, setSearchVideos] = useState([]);
+  const { searchQuery } = useContext(SearchContext);
+  const { setIsToggled } = useContext(SideBarContext);
 
-export default SearchedVideoPage
+  const loadVideos = useCallback(async () => {
+    setIsToggled(true);
+    const videos = await getVideoInfo(searchQuery.videos);
+    setSearchVideos(videos);
+  }, [searchQuery.videos, setIsToggled]);
+
+  const searchedVideoMarkup = searchVideos?.map((video) => (
+    <VideoCard
+      key={video.id.videoId}
+      id={video.id.videoId}
+      video={video}
+      img={video.snippet.thumbnails.medium.url}
+      info={video.snippet}
+      extraInfo={video.extraInfo}
+      channelInfo={video.channelInfo}
+    />
+  ));
+
+  useEffect(() => {
+    loadVideos();
+  }, [loadVideos]);
+
+  return <section className="searchedVideos">{searchedVideoMarkup}</section>;
+};
+
+export default SearchedVideosPage;
